d534c0298e64659d84fdca389975d2ab0deb68fe
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2017-01-27 07:18:53 -0500

    finalise 2.1

--- fp.rkt
+++ fp.rkt
@@ -34,7 +34,7 @@
 		((list? (car  L)) (+ (sum-up-numbers-simple (cdr L)) 0 ))
 		
 		;if element is a number, add it with the rest of the list recursively
-		((number? (car L)) (+ (sum-up-numbers-simple (cdr L)) (car L))
+		((number? (car L)) (+ (sum-up-numbers-simple (cdr L)) (car L)))
 		
 		;else, the element is not a number, just sum remaining element in list 
 		(else (+ (sum-up-numbers-simple (cdr L)) 0))
